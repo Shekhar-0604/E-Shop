af8ac69c66639500fb32b4cca7fedff8e834ae25
Shekhar <Shekhar209203@gmail.com>
2024-05-23 18:11:31 +0530

    added few components

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -1,10 +1,21 @@
 import React from "react";
 import Layout from "../../components/layout/Layout";
+import HeroSection from "../../components/heroSection/HeroSection";
+import Category from "../../components/category/Category";
+import HomePageProductCard from "../../components/homePageProductCard/HomePageProductCard";
+import Track from "../../components/track/Track";
+import Testimonial from "../../components/testimonial/Testimonial";
 
 function HomePage() {
   return (
     <div>
-      <Layout />
+      <Layout>
+        <HeroSection />
+        <Category />
+        <HomePageProductCard />
+        <Track />
+        <Testimonial />
+      </Layout>
     </div>
   );
 }
