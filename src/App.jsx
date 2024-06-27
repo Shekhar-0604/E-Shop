db6b088f747afb26a6c9f4c43d838bfc67909388
Shekhar <Shekhar209203@gmail.com>
2024-06-27 11:55:24 +0530

    change

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,18 @@ import "./App.css";
 import { Button } from "@material-tailwind/react";
 import HomePage from "./pages/home/HomePage";
 import NoPage from "./pages/noPage/NoPage";
+import ProductInfo from "./pages/productInfo/ProductInfo";
+import ScrollTop from "./components/scrollTop/ScrollTop";
 
 function App() {
   return (
     <>
       <Router>
+        <ScrollTop />
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/*" element={<NoPage />} />
+          <Route path="/productinfo" element={<ProductInfo />} />
         </Routes>
       </Router>
     </>
